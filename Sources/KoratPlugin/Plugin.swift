22901b4b7d9dee1ad4af067b3d890d9c8e59b8f7
k-ymmt <account@kymmt.net>
2020-02-24 14:19:40 +0900

    Update: add name property

--- Sources/KoratPlugin/Plugin.swift
+++ Sources/KoratPlugin/Plugin.swift
@@ -2,6 +2,8 @@ import Foundation
 import AppKit
 
 public protocol Plugin: NSObject {
+    var name: String { get  }
+    
     init(korat: Korat)
     func loadViewController() -> NSViewController
 }
